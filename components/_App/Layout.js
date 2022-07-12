f175f12cc611e5404e0d7d35462bd2c5f0e01b21
WEB/BLOCKCHAIN WORLD <92005974+topcoder728@users.noreply.github.com>
2022-07-13 01:30:06 +0900

    feat: metadata

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -8,9 +8,9 @@ const Layout = ({ children }) => {
             <Head>
                 <title>Telemedicine - Medical Health & Doctors</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                <meta name="description" content="Telecarelife - React Next Medical Health & Doctors" />
-                <meta name="og:title" property="og:title" content="Telecarelife - React Next Medical Health & Doctors"></meta>
-                <meta name="twitter:card" content="Telecarelife - React Next Medical Health & Doctors"></meta>
+                <meta name="description" content="Telecarelife - Telemedicine with  anxiety & depression, hypertension, prescription refills, antidepressants, sleeping pills, insomnia, PTSD, erectile dysfunction, relationship conflict , marriage counseling, diabetes, allergy pills, Covid testing, Covid treatment, doctor’s note for work" />
+                <meta name="og:title" property="og:title" content="Telecarelife - Medical Health & Doctors"></meta>
+                <meta name="twitter:card" content="Telecarelife - Telemedicine & Doctors"></meta>
                 <link rel="canonical" href="http://localhost:3000/"></link>
             </Head>
 
